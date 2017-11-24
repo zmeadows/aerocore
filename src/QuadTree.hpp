7694a2d027325a633d1ef38d2908e9b77d6a44dc
zmeadows <zmeadows@gmail.com>
2017-11-24 11:15:39 -0500

    added initial skeleton of eventual QuadTree class for efficient broad phase collision detection

---
+++ src/QuadTree.hpp
@@ -0,0 +1,39 @@
+#pragma once
+
+#include <array>
+#include <memory>
+#include <set>
+
+#include "Base.hpp"
+#include "UUID.hpp"
+
+class QuadTree {
+private:
+    struct Node {
+        const unsigned depth;
+        const float x, y, w;
+
+        Node* parent;
+
+        std::array<std::unique_ptr<Node>, 4> children;
+
+        std::set<UUID> containedUUIDs;
+
+        bool inBounds(const Position& pos) const;
+        bool hasChildren(void) const { return children[0] != nullptr; }
+
+        Node(const unsigned depth_) : depth(depth_) {}
+    };
+
+    Node top;
+
+    const float minimumNodeWidth;
+
+    Node* insert(const UUID& uuid, const Position& pos);
+    Node* update(const UUID& uuid, Node* currentNode, const Position& newPos);
+
+    // std::vector<UUID> collisionCandidates (across all nodes)
+public:
+};
+
+bool QuadTree::Node::inBounds(const Position& pos) const { return false; }
