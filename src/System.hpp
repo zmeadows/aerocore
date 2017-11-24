9cc28415e3c08124847f1b4fcc1a1267467f6877
zmeadows <zmeadows@gmail.com>
2017-11-24 13:46:05 -0500

    add time interval to system 'run' method

--- src/System.hpp
+++ src/System.hpp
@@ -1,39 +1,37 @@
 #pragma once
 
-#include <set>
 #include <memory>
+#include <set>
 
 #include "UUID.hpp"
 
 class ComponentManager;
 
 class System {
-private:
-  std::set<UUID> m_followed;
-
 protected:
-  ComponentManager* const CM;
-  virtual void runInternal(const UUID& uuid) = 0;
+    std::set<UUID> m_followed;
 
-  System(const std::string &name_, ComponentManager *const CM_)
-      : name(name_), CM(CM_) {}
+    ComponentManager* const CM;
+
+    System(const std::string& name_, ComponentManager* const CM_)
+        : CM(CM_), name(name_) {}
 
 public:
-  void run(void) {
-    for (const UUID& uuid : m_followed)
-      runInternal(uuid);
-  }
+    virtual void run(float dt) = 0;
 
-  const std::string name;
+    const std::string name;
 
-  bool isFollowing(const UUID &uuid) const {
-    return static_cast<bool>(m_followed.count(uuid));
-  }
+    bool isFollowing(const UUID& uuid) const {
+        return static_cast<bool>(m_followed.count(uuid));
+    }
 
-  void follow(const UUID& uuid) {
-     m_followed.insert(uuid);
-     DEBUG (name << " system followed UUID: " << uuid);
-  }
+    void follow(const UUID& uuid) {
+        m_followed.insert(uuid);
+        DEBUG(name << " system followed UUID: " << uuid);
+    }
 
-  void unfollow(const UUID& uuid) { m_followed.erase(uuid); }
+    void unfollow(const UUID& uuid) {
+        m_followed.erase(uuid);
+        DEBUG(name << " system unfollowed UUID: " << uuid);
+    }
 };
