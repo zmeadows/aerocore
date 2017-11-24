bea8e48ed629cd62e8d6f8a01038e523703d9986
zmeadows <zmeadows@gmail.com>
2017-11-24 11:16:04 -0500

    updated velocity to reflect new coordinate system

--- src/InputManager.hpp
+++ src/InputManager.hpp
@@ -3,9 +3,8 @@
 #include <cassert>
 
 #include "ComponentManager.hpp"
-#include "UUID.hpp"
 #include "Generator.hpp"
-
+#include "UUID.hpp"
 
 class InputManager {
     ComponentManager* const CM;
@@ -16,56 +15,61 @@ public:
 };
 
 void InputManager::processInput(SDL_Keycode key, bool keyUp) {
-  auto vel = CM->get<Velocity>(UUID::playerUUID);
+    auto vel = CM->get<Velocity>(UUID::playerUUID);
 
-  assert(vel != nullptr);
+    assert(vel != nullptr);
 
-  if (!keyUp) {
-    switch (key) {
-    case SDLK_UP: {
-      vel->y = -1;
-      break;
-    }
-    case SDLK_DOWN: {
-      vel->y = 1;
-      break;
-    }
-    case SDLK_RIGHT: {
-      vel->x = 1;
-      break;
-    }
-    case SDLK_LEFT: {
-      vel->x = -1;
-      break;
-    }
-    case SDLK_SPACE: {
-      UUID bulletUUID = generate<EntityType::Bullet>(CM);
-      auto playerPos = CM->get<Position>(UUID::playerUUID);
+    if (!keyUp) {
+        switch (key) {
+        case SDLK_UP: {
+            vel->y = 100;
+            break;
+        }
+        case SDLK_DOWN: {
+            vel->y = -100;
+            break;
+        }
+        case SDLK_RIGHT: {
+            vel->x = 100;
+            break;
+        }
+        case SDLK_LEFT: {
+            vel->x = -100;
+            break;
+        }
+        case SDLK_SPACE: {
+            // UUID bulletUUID = generate<EntityType::Bullet>(CM);
+            // auto const playerPos = CM->get<Position>(UUID::playerUUID);
 
-      CM->add<Position>(bulletUUID, {playerPos->x, playerPos->y - 25});
-      CM->add<Velocity>(bulletUUID, {0, -1});
-      break;
+            // CM->add<Alliance>(bulletUUID, Alliance::Friend);
+
+            // auto pos = CM->get<Position>(bulletUUID);
+            // pos->x = playerPos->x;
+            // pos->y = playerPos->y + 25;
+
+            // CM->add<Velocity>(bulletUUID, {0, -4});
+            break;
+        }
+        default: { break; }
+        }
+    } else {
+        switch (key) {
+        case SDLK_UP:
+            vel->y = 0;
+            break;
+        case SDLK_DOWN:
+            vel->y = 0;
+            break;
+        case SDLK_RIGHT:
+            vel->x = 0;
+            break;
+        case SDLK_LEFT:
+            vel->x = 0;
+            break;
+        case SDLK_SPACE:
+            break;
+        default:
+            break;
+        }
     }
-    default: { break; }
-    }
-  } else {
-    switch (key) {
-    case SDLK_UP:
-      vel->y = 0;
-      break;
-    case SDLK_DOWN:
-      vel->y = 0;
-      break;
-    case SDLK_RIGHT:
-      vel->x = 0;
-      break;
-    case SDLK_LEFT:
-      vel->x = 0;
-      break;
-    case SDLK_SPACE:
-      break;
-    default:
-      break;
-    }
-  }
 }
