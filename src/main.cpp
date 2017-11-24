98d9fcc998427301c726ba98d1c9e8de7990b776
zmeadows <zmeadows@gmail.com>
2017-11-24 13:47:16 -0500

    sending update time to System.run method

--- src/main.cpp
+++ src/main.cpp
@@ -1,60 +1,50 @@
 #include <SDL2/SDL.h>
 
-#include "UUID.hpp"
+#include "Base.hpp"
 #include "ComponentManager.hpp"
-#include "SystemManager.hpp"
+#include "Generator.hpp"
+#include "GraphicsContext.hpp"
 #include "InputManager.hpp"
-#include "System.hpp"
 #include "Sprite.hpp"
+#include "System.hpp"
+#include "SystemManager.hpp"
+#include "UUID.hpp"
 
-int main (int argc, char** argv)
-{
-    SDL_Init( SDL_INIT_VIDEO );
+int main(int argc, char** argv) {
 
-    SDL_Window* window = nullptr;
-
-    window = SDL_CreateWindow(
-      "aerocore",
-      SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 800,
-      SDL_WINDOW_SHOWN | SDL_WINDOW_BORDERLESS | SDL_WINDOW_ALLOW_HIGHDPI
-    );
-    
-    // Setup renderer
-    SDL_Renderer* renderer = nullptr;
-    renderer =  SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-
+    auto GC = std::make_unique<GraphicsContext>();
     auto CM = std::make_unique<ComponentManager>();
-    auto SM = std::make_unique<SystemManager>(CM.get(), renderer);
+    auto SM = std::make_unique<SystemManager>(CM.get(), GC.get());
     auto IM = std::make_unique<InputManager>(CM.get());
 
-    CM->add<Position>(UUID::playerUUID, {50,50});
-    CM->add<Velocity>(UUID::playerUUID, {0,0});
-    CM->add<Sprite>(UUID::playerUUID, new SquareSprite(50));
+    generate<EntityType::Player>(CM.get());
+    generate<EntityType::Enemy>(CM.get());
 
     SDL_Event e;
 
     bool quit = false;
 
+    Uint64 t1, t0 = SDL_GetPerformanceCounter();
+
     while (!quit) {
-      while (SDL_PollEvent(&e) != 0) {
-        if (e.type == SDL_QUIT) {
-          quit = true;
-        } else if (e.type == SDL_KEYDOWN) {
-            IM->processInput(e.key.keysym.sym, false);
-        } else if (e.type == SDL_KEYUP) {
-            IM->processInput(e.key.keysym.sym, true);
+
+        while (SDL_PollEvent(&e) != 0) {
+            if (e.type == SDL_QUIT) {
+                quit = true;
+            } else if (e.type == SDL_KEYDOWN && e.key.repeat == 0) {
+                IM->processInput(e.key.keysym.sym, false);
+            } else if (e.type == SDL_KEYUP && e.key.repeat == 0) {
+                IM->processInput(e.key.keysym.sym, true);
+            }
         }
-      }
 
-      SDL_SetRenderDrawColor(renderer, 25, 25, 25, 255);
-      SDL_RenderClear(renderer);
-      SM->runSystems();
-      SDL_RenderPresent(renderer);
-      SDL_Delay(10);
+        SDL_SetRenderDrawColor(GC->renderer, 225, 225, 225, 255);
+        SDL_RenderClear(GC->renderer);
+        t1 = SDL_GetPerformanceCounter();
+        SM->runSystems((double)(t1 - t0) / SDL_GetPerformanceFrequency());
+        t0 = SDL_GetPerformanceCounter();
+        SDL_RenderPresent(GC->renderer);
     }
-
-    SDL_DestroyWindow(window);
-    SDL_Quit();
 
     return EXIT_SUCCESS;
 }
