c86e1e9928716e7852bec86f3fb0d8227ae7352f
zmeadows <zmeadows@gmail.com>
2017-11-24 11:23:09 -0500

    adding Alliances and new coordinates

--- src/Generator.hpp
+++ src/Generator.hpp
@@ -2,29 +2,53 @@
 
 #include "ComponentManager.hpp"
 
-template <EntityType EType>
-UUID generate(ComponentManager* const CM) {
+template <EntityType EType> UUID generate(ComponentManager* const CM) {
     UUID newUUID;
     return newUUID;
 }
 
-template<>
-UUID generate<EntityType::Player>(ComponentManager* const CM) {
-    CM->add<Position>(UUID::playerUUID, {50,50});
-    CM->add<Velocity>(UUID::playerUUID, {0,0});
-    CM->add<Sprite>(UUID::playerUUID, new SquareSprite(50));
+template <> UUID generate<EntityType::Player>(ComponentManager* const CM) {
+    CM->add<Position>(UUID::playerUUID, {0, 0});
+    CM->add<Velocity>(UUID::playerUUID, {0, 0});
+    CM->add<Alliance>(UUID::playerUUID, Alliance::Friend);
+
+    CM->add<Sprite>(UUID::playerUUID, new SquareSprite(10.0));
+    auto const spr = CM->get<Sprite>(UUID::playerUUID);
+    CM->add<BoundingSurface>(UUID::playerUUID, spr->buildBoundingSurface());
+
     return UUID::playerUUID;
 }
 
-template<>
-UUID generate<EntityType::Bullet>(ComponentManager* const CM) {
+template <> UUID generate<EntityType::Bullet>(ComponentManager* const CM) {
     UUID bulletUUID;
 
-    auto bulletSprite = new SquareSprite(25);
-    bulletSprite->rgba.r = 200;
-    bulletSprite->rgba.g = 200;
-    bulletSprite->rgba.b = 200;
+    // auto bulletSprite = new SquareSprite(25);
+    // bulletSprite->rgba.r = 0;
+    // bulletSprite->rgba.g = 200;
+    // bulletSprite->rgba.b = 0;
 
-    CM->add<Sprite>(bulletUUID, bulletSprite);
+    // CM->add<Sprite>(bulletUUID, bulletSprite);
+
+    // CM->add<Position>(bulletUUID, Position());
+
+    // auto const pos = CM->get<Position>(bulletUUID);
+    // CM->add<BoundingSurface>(bulletUUID, new BoundingBox(25, pos));
+
     return bulletUUID;
+}
+
+template <> UUID generate<EntityType::Enemy>(ComponentManager* const CM) {
+    UUID enemyUUID;
+    CM->add<Position>(enemyUUID, {75, 0});
+    CM->add<Velocity>(enemyUUID, {0, 0});
+
+    auto spr = new SquareSprite(10.0);
+    spr->rgba.r = 255;
+    spr->rgba.g = 0;
+    CM->add<Sprite>(enemyUUID, spr);
+    CM->add<Alliance>(enemyUUID, Alliance::Foe);
+
+    CM->add<BoundingSurface>(enemyUUID, spr->buildBoundingSurface());
+
+    return enemyUUID;
 }
