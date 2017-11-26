c94cdc47db6e08482668f71ff2d2cb3582caa5c8
zmeadows <zmeadows@gmail.com>
2017-11-26 14:40:37 -0500

    inlined template specializations

--- src/Generator.hpp
+++ src/Generator.hpp
@@ -1,15 +1,19 @@
 #pragma once
 
+#include "Base.hpp"
 #include "ComponentManager.hpp"
 
-template <EntityType EType> UUID generate(ComponentManager* const CM) {
+template <EntityType EType>
+UUID generate(ComponentManager* const CM) {
     UUID newUUID;
     return newUUID;
 }
 
-template <> UUID generate<EntityType::Player>(ComponentManager* const CM) {
+template <>
+inline UUID generate<EntityType::Player>(ComponentManager* const CM) {
     CM->add<Position>(UUID::playerUUID, {0, 0});
     CM->add<Velocity>(UUID::playerUUID, {0, 0});
+    CM->add<Acceleration>(UUID::playerUUID, {0, 0});
     CM->add<Alliance>(UUID::playerUUID, Alliance::Friend);
 
     CM->add<Sprite>(UUID::playerUUID, new SquareSprite(10.0));
@@ -19,28 +23,31 @@ template <> UUID generate<EntityType::Player>(ComponentManager* const CM) {
     return UUID::playerUUID;
 }
 
-template <> UUID generate<EntityType::Bullet>(ComponentManager* const CM) {
+template <>
+inline UUID generate<EntityType::Bullet>(ComponentManager* const CM) {
     UUID bulletUUID;
 
-    // auto bulletSprite = new SquareSprite(25);
-    // bulletSprite->rgba.r = 0;
-    // bulletSprite->rgba.g = 200;
-    // bulletSprite->rgba.b = 0;
+    auto bulletSprite = new SquareSprite(2);
+    bulletSprite->rgba.r = 0;
+    bulletSprite->rgba.g = 200;
+    bulletSprite->rgba.b = 0;
+    CM->add<Sprite>(bulletUUID, bulletSprite);
 
-    // CM->add<Sprite>(bulletUUID, bulletSprite);
+    CM->add<Position>(bulletUUID, Position());
+    CM->add<Velocity>(bulletUUID, {0, 150});
+    CM->add<Acceleration>(bulletUUID, {0, 0});
 
-    // CM->add<Position>(bulletUUID, Position());
-
-    // auto const pos = CM->get<Position>(bulletUUID);
-    // CM->add<BoundingSurface>(bulletUUID, new BoundingBox(25, pos));
+    CM->add<BoundingSurface>(bulletUUID, bulletSprite->buildBoundingSurface());
 
     return bulletUUID;
 }
 
-template <> UUID generate<EntityType::Enemy>(ComponentManager* const CM) {
+template <>
+inline UUID generate<EntityType::Enemy>(ComponentManager* const CM) {
     UUID enemyUUID;
     CM->add<Position>(enemyUUID, {75, 0});
     CM->add<Velocity>(enemyUUID, {0, 0});
+    CM->add<Acceleration>(enemyUUID, {0, 0});
 
     auto spr = new SquareSprite(10.0);
     spr->rgba.r = 255;
