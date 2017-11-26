119d86b6f6b9b47075df05bdb19c3e2d44752360
zmeadows <zmeadows@gmail.com>
2017-11-26 14:43:30 -0500

    factored implementation out into InputManager.cpp

--- src/InputManager.hpp
+++ src/InputManager.hpp
@@ -1,75 +1,31 @@
 #pragma once
 
 #include <cassert>
+#include <functional>
+#include <unordered_map>
 
-#include "ComponentManager.hpp"
-#include "Generator.hpp"
-#include "UUID.hpp"
+#include <SDL2/SDL.h>
+
+class ComponentManager;
+
+enum class Key { LeftArrow, RightArrow, UpArrow, DownArrow, Spacebar };
+enum class KeyState { Pressed, Released };
 
 class InputManager {
     ComponentManager* const CM;
 
+    std::unordered_map<Key, KeyState> m_keyStates;
+
+    void processPressedKey(const Key& key);
+    void processReleasedKey(const Key& key);
+
 public:
-    InputManager(ComponentManager* const CM_) : CM(CM_) {}
-    void processInput(SDL_Keycode key, bool keyUp);
-};
-
-void InputManager::processInput(SDL_Keycode key, bool keyUp) {
-    auto vel = CM->get<Velocity>(UUID::playerUUID);
-
-    assert(vel != nullptr);
-
-    if (!keyUp) {
-        switch (key) {
-        case SDLK_UP: {
-            vel->y = 100;
-            break;
-        }
-        case SDLK_DOWN: {
-            vel->y = -100;
-            break;
-        }
-        case SDLK_RIGHT: {
-            vel->x = 100;
-            break;
-        }
-        case SDLK_LEFT: {
-            vel->x = -100;
-            break;
-        }
-        case SDLK_SPACE: {
-            // UUID bulletUUID = generate<EntityType::Bullet>(CM);
-            // auto const playerPos = CM->get<Position>(UUID::playerUUID);
-
-            // CM->add<Alliance>(bulletUUID, Alliance::Friend);
-
-            // auto pos = CM->get<Position>(bulletUUID);
-            // pos->x = playerPos->x;
-            // pos->y = playerPos->y + 25;
-
-            // CM->add<Velocity>(bulletUUID, {0, -4});
-            break;
-        }
-        default: { break; }
-        }
-    } else {
-        switch (key) {
-        case SDLK_UP:
-            vel->y = 0;
-            break;
-        case SDLK_DOWN:
-            vel->y = 0;
-            break;
-        case SDLK_RIGHT:
-            vel->x = 0;
-            break;
-        case SDLK_LEFT:
-            vel->x = 0;
-            break;
-        case SDLK_SPACE:
-            break;
-        default:
-            break;
-        }
+    InputManager(ComponentManager* const CM_) : CM(CM_) {
+        m_keyStates[Key::LeftArrow] = KeyState::Released;
+        m_keyStates[Key::RightArrow] = KeyState::Released;
+        m_keyStates[Key::DownArrow] = KeyState::Released;
+        m_keyStates[Key::UpArrow] = KeyState::Released;
+        m_keyStates[Key::Spacebar] = KeyState::Released;
     }
-}
+    void processInput(SDL_Keycode SDLkey, bool keyUp);
+};
