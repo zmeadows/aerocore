b1d180c0827c3b56237b30cade9057824d92ced6
Zachary Meadows <zmeadows@gmail.com>
2019-01-04 10:58:51 -0600

    add basic enemy skeleton function

--- src/Generator/Enemy.cpp
+++ src/Generator/Enemy.cpp
@@ -4,32 +4,83 @@
 #include "Entity.hpp"
 #include "Sprite.hpp"
 
-void generateStabber(void) {
-    UUID uuid;
+#include <memory>
+#include <unordered_map>
+
+// void generateStabber(void) {
+//     UUID uuid;
+//     auto CM = get_manager();
+//
+//     Entity& entity = CM->book<Entity>(uuid);
+//     CM->book<EulerTranslation>(uuid);
+//     CM->book<EulerRotation>(uuid);
+//     CM->book<PositionUpdate>(uuid);
+//     CM->book<RotationUpdate>(uuid);
+//     auto& cd = CM->book<CollisionData>(uuid);
+//     auto& sprite = CM->book<Sprite>(uuid);
+//
+//     entity.pos = { uniform_rand(-80.f, 80.f), uniform_rand(-80.f, 80.f) };
+//
+//     auto parse_result = parse_svg_path(asset_path("sprites/stabber/path").c_str());
+//     cd.local_vertices = parse_result.vertices;
+//     sprite.offset = parse_result.sprite_offset;
+//     auto stsprite = GPU_LoadImage(asset_path("sprites/stabber/stabber.png").c_str());
+//     sprite.image = stsprite;
+//
+//     entity.angle = uniform_rand(0.f, PI/2.f - 1e-3f);
+//     populate_global_vertices(cd.local_vertices, cd.global_vertices, entity.pos, entity.angle);
+//     entity.extent = extent_of(cd.global_vertices);
+//     cd.poly_decomp = decompose_local_vertices(cd.local_vertices);
+//     cd.node = get_quad_tree()->insert_entity(playerUUID(), clip_to_screen(entity.extent));
+//
+//     auto& stabber = CM->book<Stabber>(uuid);
+//     stabber.speed = uniform_rand(75,100);
+// }
+
+namespace {
+
+struct ImagePtrDeleter {
+    void operator()(GPU_Image* p) { GPU_FreeImage(p); }
+};
+
+using ImagePtr = std::unique_ptr<GPU_Image, ImagePtrDeleter>;
+
+std::unordered_map<std::string, ImagePtr> sprite_cache;
+
+GPU_Image* make_sprite(const std::string& image_path) {
+    auto it = sprite_cache.find(image_path);
+    if (it != sprite_cache.end()) {
+        return it->second.get();
+    } else {
+        auto image = GPU_LoadImage(image_path.c_str());
+        sprite_cache.emplace(image_path, image);
+        return image;
+    }
+}
+
+}
+
+void generate_enemy_skeleton(UUID uuid, v2 position, const char* sprite_file, const char* path_file) {
     auto CM = get_manager();
 
     Entity& entity = CM->book<Entity>(uuid);
-    CM->book<EulerTranslation>(uuid);
-    CM->book<EulerRotation>(uuid);
+    entity.pos = position;
+    entity.angle = 0.f;
     CM->book<PositionUpdate>(uuid);
     CM->book<RotationUpdate>(uuid);
     auto& cd = CM->book<CollisionData>(uuid);
     auto& sprite = CM->book<Sprite>(uuid);
 
-    entity.pos = { uniform_rand(-80.f, 80.f), uniform_rand(-80.f, 80.f) };
+    auto& osb = CM->book<OffscreenBehavior>(uuid);
 
-    auto parse_result = parse_svg_path(asset_path("sprites/stabber/path").c_str());
+    auto parse_result = parse_svg_path(asset_path(path_file).c_str());
     cd.local_vertices = parse_result.vertices;
     sprite.offset = parse_result.sprite_offset;
-    auto stsprite = GPU_LoadImage(asset_path("sprites/stabber/stabber.png").c_str());
-    sprite.image = stsprite;
+    sprite.image = make_sprite(asset_path(sprite_file));
 
-    entity.angle = uniform_rand(0.f, PI/2.f - 1e-3f);
     populate_global_vertices(cd.local_vertices, cd.global_vertices, entity.pos, entity.angle);
     entity.extent = extent_of(cd.global_vertices);
     cd.poly_decomp = decompose_local_vertices(cd.local_vertices);
-    cd.node = get_quad_tree()->insert_entity(playerUUID(), clip_to_screen(entity.extent));
+    cd.node = get_quad_tree()->insert_entity(uuid, clip_to_screen(entity.extent));
 
-    auto& stabber = CM->book<Stabber>(uuid);
-    stabber.speed = uniform_rand(75,100);
 }
--- src/Generator/Enemy.hpp
+++ src/Generator/Enemy.hpp
@@ -2,17 +2,19 @@
 
 #include "Vector2D.hpp"
 
-void generateStabber(void);
+void generate_enemy_skeleton(UUID uuid, v2 position, const char* sprite_file, const char* path_file);
 
-struct Stabber {
-    enum class State {
-        Stabbing,
-        Relocating,
-        Stopping,
-        Aiming
-    } state = State::Stopping;
+// void generateStabber(void);
 
-    float speed = 0.f;
-
-    v2 idle_point;
-};
+// struct Stabber {
+//     enum class State {
+//         Stabbing,
+//         Relocating,
+//         Stopping,
+//         Aiming
+//     } state = State::Stopping;
+//
+//     float speed = 0.f;
+//
+//     v2 idle_point;
+// };
