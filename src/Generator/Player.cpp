e02826e5a8810d13343163826108b507e4119dd2
Zachary Meadows <zmeadows@gmail.com>
2019-01-04 13:57:49 -0600

    update bullet gen code to use generate_bullet_player

--- src/Generator/Player.cpp
+++ src/Generator/Player.cpp
@@ -1,4 +1,5 @@
 #include "Generator/Player.hpp"
+#include "Generator/Bullet.hpp"
 #include "Geometry.hpp"
 #include "Globals.hpp"
 #include "Entity.hpp"
@@ -7,7 +8,6 @@
 #include <random>
 
 void generatePlayer() {
-
     ComponentManager* CM = get_manager();
 
     Entity& player = CM->book<Entity>(playerUUID());
@@ -19,6 +19,8 @@ void generatePlayer() {
     auto& cd = CM->book<CollisionData>(playerUUID());
     auto& sprite = CM->book<Sprite>(playerUUID());
 
+    player.angle = PI/2.0;
+
     auto parse_result = parse_svg_path(asset_path("sprites/player/path").c_str());
     cd.local_vertices = parse_result.vertices;
     sprite.offset = parse_result.sprite_offset;
@@ -26,10 +28,27 @@ void generatePlayer() {
     populate_global_vertices(cd.local_vertices, cd.global_vertices, player.pos, player.angle);
     player.extent = extent_of(cd.global_vertices);
     cd.node = get_quad_tree()->insert_entity(playerUUID(), clip_to_screen(player.extent));
-    assert(player.node);
+    assert(cd.node);
 
     sprite.image = GPU_LoadImage(asset_path("sprites/player/player.png").c_str());
 
     auto& osb = CM->book<OffscreenBehavior>(playerUUID());
     osb.type = OffscreenBehavior::Type::Wraps;
+}
+
+void generatePlayerBullet(void) {
+    auto CM = get_manager();
+
+    const auto& player = CM->get<Entity>(playerUUID());
+    const v2& player_pos = player.pos;
+    const v2& player_orientation = orientation_of(player);
+
+    v2 bullet_pos, bullet_vel;
+
+    v2 bullet_pos_offset = 4.0 * player_orientation;
+
+    bullet_pos = player_pos + bullet_pos_offset;
+    bullet_vel = 200.f * orientation_of(player);
+
+    generate_bullet_player(bullet_pos, bullet_vel, player.angle);
 }
--- src/Generator/Player.hpp
+++ src/Generator/Player.hpp
@@ -1,3 +1,4 @@
 #pragma once
 
 void generatePlayer(void);
+void generatePlayerBullet(void);
