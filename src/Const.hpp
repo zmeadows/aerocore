3bf1f14c0c6f6d9d8c187688c0afd15a57875f9f
Zachary Meadows <zmeadows@gmail.com>
2019-01-04 10:57:03 -0600

    hard-coded pi values

--- src/Const.hpp
+++ src/Const.hpp
@@ -2,5 +2,5 @@
 
 #include <math.h>
 
-const float TWOPI = 2.f * static_cast<float>(M_PI);
-const float PI = static_cast<float>(M_PI);
+const float TWOPI = 2.f * static_cast<float>(3.1415926535897932384);
+const float PI = static_cast<float>(3.1415926535897932384);
