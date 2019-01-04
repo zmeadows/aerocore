af2ace89344d8388509368dd2dfec7b99d80d691
Zachary Meadows <zmeadows@gmail.com>
2019-01-04 10:55:37 -0600

    friendly/nonfriendly bullet generation

--- src/Generator/Bullet.cpp
+++ src/Generator/Bullet.cpp
@@ -1,39 +1,59 @@
 #include "Generator/Bullet.hpp"
+#include "Generator/Enemy.hpp"
 #include "Geometry.hpp"
 #include "Globals.hpp"
 #include "Entity.hpp"
 #include "Sprite.hpp"
 
-UUID generateBullet(const v2 new_bullet_position, const v2 new_bullet_velocity, float new_bullet_angle) {
-    UUID bulletUUID;
+UUID generate_bullet_player(const v2 new_bullet_position,
+                            const v2 new_bullet_velocity,
+                            float new_bullet_angle)
+{
+    UUID uuid;
+
+    generate_enemy_skeleton(uuid,
+                            new_bullet_position,
+                            "sprites/playerbullet/playerbullet.png",
+                            "sprites/playerbullet/path"
+                            );
 
     auto CM = get_manager();
+    CM->book<FriendlyTag>(uuid);
 
-    Entity& bullet = CM->book<Entity>(bulletUUID);
-    EulerTranslation& kin = CM->book<EulerTranslation>(bulletUUID);
-    CM->book<PositionUpdate>(bulletUUID);
-    CM->book<RotationUpdate>(bulletUUID);
-    CM->book<OffscreenBehavior>(bulletUUID);
-    auto& cd = CM->book<CollisionData>(bulletUUID);
-    auto& sprite = CM->book<Sprite>(bulletUUID);
-
-    CM->book<FriendlyTag>(bulletUUID);
+    auto& bullet = CM->get<Entity>(uuid);
+    auto& kin = CM->book<EulerTranslation>(uuid);
 
     bullet.pos = new_bullet_position;
     bullet.angle = new_bullet_angle;
     kin.vel = new_bullet_velocity;
 
-    auto parse_result = parse_svg_path(asset_path("sprites/playerbullet/path").c_str());
-    cd.local_vertices = parse_result.vertices;
-    sprite.offset = parse_result.sprite_offset;
-    cd.poly_decomp = decompose_local_vertices(cd.local_vertices);
-    populate_global_vertices(cd.local_vertices, cd.global_vertices, bullet.pos, bullet.angle);
-    bullet.extent = extent_of(cd.global_vertices);
-    cd.node = get_quad_tree()->insert_entity(playerUUID(), clip_to_screen(bullet.extent));
+    play_sound(SoundEffect_ShotFired);
 
-    sprite.image = GPU_LoadImage(asset_path("sprites/playerbullet/playerbullet.png").c_str());
+    return uuid;
+}
+
+UUID generate_bullet_enemy(const v2 new_bullet_position,
+                           const v2 new_bullet_velocity,
+                           float new_bullet_angle)
+{
+    UUID uuid;
+
+    generate_enemy_skeleton(uuid,
+                            new_bullet_position,
+                            "sprites/playerbullet/playerbullet.png",
+                            "sprites/playerbullet/path"
+                            );
+
+    auto CM = get_manager();
+
+    auto& bullet = CM->get<Entity>(uuid);
+    auto& kin = CM->book<EulerTranslation>(uuid);
+
+    bullet.pos = new_bullet_position;
+    bullet.angle = new_bullet_angle;
+    kin.vel = new_bullet_velocity;
 
     play_sound(SoundEffect_ShotFired);
 
-    return bulletUUID;
+    return uuid;
 }
--- src/Generator/Bullet.hpp
+++ src/Generator/Bullet.hpp
@@ -3,4 +3,5 @@
 struct v2;
 class UUID;
 
-UUID generateBullet(const v2 new_bullet_position, const v2 new_bullet_velocity, float new_bullet_angle);
+UUID generate_bullet_player(const v2 new_bullet_position, const v2 new_bullet_velocity, float new_bullet_angle);
+UUID generate_bullet_enemy(const v2 new_bullet_position, const v2 new_bullet_velocity, float new_bullet_angle);
