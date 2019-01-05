82b21b911a56bfb454129aabf7e0273e4f5ae45c
Zachary Meadows <zmeadows@gmail.com>
2019-01-05 07:42:28 -0600

    added fast random number generator

---
+++ src/Random.hpp
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <cstdint>
+#include <limits>
+
+namespace {
+    uint_fast32_t x=123456789, y=362436069, z=521288629;
+
+    //period 2^96-1
+    inline uint_fast32_t xorshf96(void) {
+        uint_fast32_t t;
+        x ^= x << 16;
+        x ^= x >> 5;
+        x ^= x << 1;
+
+        t = x;
+        x = y;
+        y = z;
+        z = t ^ x ^ y;
+
+        return z;
+    }
+}
+
+inline f32 uniform_random(f32 min_, f32 max_) {
+    const f32 r = static_cast<f32>(xorshf96());
+    const f32 d = static_cast<f32>(std::numeric_limits<uint_fast32_t>::max());
+    return min_ + (max_ - min_) * r / d;
+}
--- src/main.cpp
+++ src/main.cpp
@@ -11,23 +11,28 @@
 #include "Game.hpp"
 #include "Util.hpp"
 #include "Spline.hpp"
+#include "random.hpp"
 
 int main(int argc, char** argv) {
     srand (static_cast<unsigned>(time(nullptr)));
 
-    std::vector<v2> ps = { {0,0}, {10,10}, {0, 12}, {10, -10}, {0,-4} };
-    std::vector<f32> ts = { 0.0, 0.5, 1.3, 3.0, 5.0 };
-
-    auto s = make_spline(ps, ts);
-
-    while (true) {
-        SplineStepResult result = s.step(0.05, false);
-
-        std::cout << result.new_point.x << " " << result.new_point.y << std::endl;
-
-        if (result.unused_dt > 0)
-            break;
+    for (auto i = 0; i < 200; i++) {
+        std::cout << uniform_random(0, 1) << std::endl;
     }
+
+    // std::vector<v2> ps = { {0,0}, {10,10}, {0, 12}, {10, -10}, {0,-4} };
+    // std::vector<f32> ts = { 0.0, 0.5, 1.3, 3.0, 5.0 };
+
+    // auto s = make_spline(ps, ts);
+
+    // while (true) {
+    //     SplineStepResult result = s.step(0.05, false);
+
+    //     std::cout << result.new_point.x << " " << result.new_point.y << std::endl;
+
+    //     if (result.unused_dt > 0)
+    //         break;
+    // }
 
 
     auto game = std::make_unique<Game>();
