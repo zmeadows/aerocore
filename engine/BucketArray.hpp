e19527e5b8e25f67776207090ef8892162727d0d
Zachary Meadows <zmeadows@gmail.com>
2019-01-28 16:32:25 -0600

    finally flushed out BucketArray

--- engine/BucketArray.hpp
+++ engine/BucketArray.hpp
@@ -1,121 +1,156 @@
 #pragma once
 
-/*
-
-#include <optional>
-#include <vector>
-#include <memory>
-
-namespace __BucketArray__Internal__ {
+#include "unstd/DynamicArray.hpp"
+#include "unstd/types.hpp"
+#include "unstd/unstdlib.hpp"
 
 template <typename T>
 struct Bucket {
-    // A memory buffer to store all the data in
-    // It is not type T[] in order to allow for leaving all the items uninitialized until filled
-    std::unique_ptr<char[]> buffer;
+    u8* data;
+    bool* occupied;
+    u16 filled_slots;
+    u16 capacity;
 
-    //TODO: use bitset representation to save space here
-    std::unique_ptr<bool[]> occupied;
+    static void destroy(Bucket<T>& self) {
+        free(self.data);
+        free(self.occupied);
+        self.data = nullptr;
+        self.occupied = nullptr;
+        self.filled_slots = 0;
+    }
 
-    const u32 capacity;
+    static Bucket<T>* allocate(u64 bucket_capacity) {
+        assert(bucket_capacity < 65000);
+        Bucket<T>* new_bucket = memalloc<Bucket<T>>(1);
+        new_bucket->data = memalloc<u8, T>(bucket_capacity);
+        new_bucket->occupied = memalloc<bool, bool>(bucket_capacity);
+        new_bucket->filled_slots = 0;
+        new_bucket->capacity = bucket_capacity;
 
-    // keep track of the count of objects in the bucket, so we can quickly skip full buckets
-    // when looking for a place to insert a new object in the parent BucketArray<T>
-    u32 count;
-
-    Bucket(u32 size) : buffer(std::make_unique<char[]>(sizeof(T) * size))
-                     , occupied(std::make_unique<bool[]>(sizeof(bool) * size))
-                     , capacity(size)
-                     , count(0)
-    {
-        for (u32 i = 0; i < capacity; i++) {
-            occupied[i] = false;
+        for (auto i = 0; i < new_bucket->capacity; i++) {
+            new_bucket->occupied[i] = false;
         }
+
+        return new_bucket;
     }
 
-    Bucket(const Bucket&) = delete;
-    Bucket(Bucket&&) = delete;
-    Bucket& operator=(const Bucket&) = delete;
-    Bucket& operator=(Bucket&&) = delete;
-
-    void release(u32 idx) {
-        assert(occupied[idx]);
-        data()[idx].~T();
-        occupied[idx] = false;
-        count--;
-    }
-
-
-    T* data(void) {
-        return reinterpret_cast<T*>(buffer.get());
-    }
-
-    ~Bucket() {
-        T* data = this->data();
-        for (auto i = 0; i < capacity; i++) {
-            if (occupied[i]) data[i].~T();
-        }
-    }
+    T& operator[](u16 idx);
+    const T& operator[](u16 idx) const;
 };
 
+
+template <typename T>
+T& Bucket<T>::operator[](u16 idx) {
+    assert(idx < this->capacity && this->occupied[idx]);
+    T* cast_data = reinterpret_cast<T*>(data);
+    return cast_data[idx];
+}
+
+template <typename T>
+const T& Bucket<T>::operator[](u16 idx) const {
+    assert(idx < this->capacity && this->occupied[idx]);
+    const T* const cast_data = reinterpret_cast<const T* const>(this->data);
+    return cast_data[idx];
+}
+
+template <typename T>
+void release_slot(Bucket<T>& self, u32 slot_idx) {
+    assert(self.occupied[slot_idx]);
+    self.occupied[idx] = false;
+    self.filled_slots--;
+}
+
+template <typename T>
+u16 find_open_slot(const Bucket<T>& self) {
+    for (s32 i = 0; i < self.capacity; i++) {
+        if (!self.occupied[i])
+            return i;
+    }
+    return self.capacity;
+}
+
+template <typename T, typename... Args>
+u16 insert_in_bucket(Bucket<T>& self, Args&&... args) {
+    const u16 new_slot_idx = find_open_slot(self);
+    assert(new_slot_idx < self.capacity);
+    T* cast_data = reinterpret_cast<T*>(self.data);
+    new ( &(cast_data[new_slot_idx]) ) T(args...);
+    self.occupied[new_slot_idx] = true;
+    self.filled_slots++;
+    return new_slot_idx;
+}
+
+template <typename T>
+bool is_full(Bucket<T>& self) {
+    return self.filled_slots == self.capacity;
 }
 
 struct BucketIndex {
-    u32 bucket;
-    u32 slot;
+    u16 bucket;
+    u16 slot;
 };
 
 template <typename T>
 struct BucketArray {
-    std::vector<std::unique_ptr<Bucket<T>>> buckets;
-    const u32 bucket_size;
+    const u64 bucket_capacity = 64;
+    DynamicArray<Bucket<T>*> buckets;
+    DynamicArray<bool> bucket_filled;
 
-    BucketArray(const u32 _bucket_size)
-        : bucket_size(_bucket_size)
-        , buckets((Bucket**) malloc(sizeof(Bucket*)))
-        , bucket_count(1)
-    {
-        buckets[0] = new Bucket(items_per_bucket, item_size_bytes);
+    static void destroy(BucketArray<T>& self) {
+        for (Bucket<T>* bucket : self.buckets)
+            Bucket<T>::destroy(*bucket);
     }
 
-    Bucket(const Bucket&) = delete;
-    Bucket(Bucket&&) = delete;
-    Bucket& operator=(const Bucket&) = delete;
-    Bucket& operator=(Bucket&&) = delete;
-
-    T& const operator[](const BucketIndex bid);
-    const T& const operator[](const BucketIndex bid) const;
-
-    template <class... Args>
-    BucketIndex insert(Args&&... args) {
-        assert(count < capacity);
-
-        for (u32 bucket_id = 0; bucket_id < buckets.size(); bucket_id++) {
-            auto& bucket = buckets[i];
-
-            if (bucket->count >= bucket->capacity)
-                continue;
-
-            for (u32 slot_id = 0; slot_id < bucket->capacity; slot_id++) {
-                if (!bucket->occupied[slot_id]) {
-                    new (&(bucket->data()[i])) T(args...);
-                    bucket->count++;
-                    return {bucket_id, slot_id};
-                }
-            }
-        }
-
-        // couldn't find an empty slot in the old buckets, so make a new bucket
-        auto& new_bucket = buckets.emplace_back(std::make_unique<Bucket<T>>(bucket_size));
-        new (&(new_bucket->data()[0])) T(args...);
-        new_bucket->count++;
-        return { buckets.size() - 1, 0 };
+    static BucketArray<T> create(u16 bucket_size) {
+        return BucketArray<T>{bucket_size};
     }
 
-    void release(BucketIndex idx) {
-        buckets[idx.bucket]->release(idx.slot);
-    }
-
+    T& operator[](const BucketIndex index);
+    const T& operator[](const BucketIndex index) const;
 };
 
-*/
+template <typename T>
+u64 size(const BucketArray<T>& self) {
+    u64 sum = 0;
+    for (const Bucket<T>* bucket : self.buckets) {
+        sum += (u64) bucket->filled_slots;
+    }
+    return sum;
+}
+
+template <typename T, typename... Args>
+const BucketIndex insert(BucketArray<T>& self, Args&&... args) {
+    for (u16 bucket_id = 0; bucket_id < self.buckets.size; bucket_id++) {
+        Bucket<T>* bucket = self.buckets[bucket_id];
+
+        if (!is_full(*bucket)) {
+            u16 slot_id = insert_in_bucket(*bucket, args...);
+            return BucketIndex{bucket_id, slot_id};
+        }
+    }
+
+    // If we reach here, we couldn't find an empty slot in the old buckets.
+    // So make a new bucket.
+    append(self.buckets, Bucket<T>::allocate(self.bucket_capacity));
+    insert_in_bucket(*(back(self.buckets)), args...);
+    return BucketIndex { (u16) (self.buckets.size - 1), 0};
+}
+
+template <typename T, class... Args>
+void remove(BucketArray<T>& self, const BucketIndex index) {
+    release_slot(self.buckets[index.bucket], index.slot);
+}
+
+template <typename T>
+inline T& BucketArray<T>::operator[](const BucketIndex index) {
+    assert(index.bucket < this->buckets.size && "BucketIndex points to invalid bucket!");
+    Bucket<T>& bucket = *(this->buckets[index.bucket]);
+    return bucket[index.slot];
+}
+
+template <typename T>
+inline const T& BucketArray<T>::operator[](const BucketIndex index) const {
+    assert(index.bucket < this->buckets.size && "BucketIndex points to invalid bucket!");
+    const Bucket<T>& bucket = *(this->buckets[index.bucket]);
+    return bucket[index.slot];
+}
